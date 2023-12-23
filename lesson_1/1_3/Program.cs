4c3823caabe977b906cee532fd63c71e8aa3ec49
roman <roma-t@bk.ru>
2023-12-23 11:02:35 +0300

    task3

---
+++ lesson_1/1_3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Сейчас я найду сумму первой и последней цифры любого трёхзначного числа");
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a/100 + a%10);
