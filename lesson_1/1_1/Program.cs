a6dcad29194023146d0b783fe83d257c352cffb9
roman <roma-t@bk.ru>
2023-12-23 11:03:33 +0300

    task3

--- lesson_1/1_1/Program.cs
+++ lesson_1/1_1/Program.cs
@@ -1,3 +1,11 @@
-﻿Console.Write("enter a number 1: ");
-int num1 = int.Parse(Console.ReadLine()!);
-Console.WriteLine(num1*num1);
+﻿//Console.Write("enter a number 1");
+//int num1 = int.Parse(Console.ReadLine()!);
+//Console.WriteLine(num1*num1);
+
+int a = 25;
+int b = 5;
+
+if (b*b == a)
+    Console.WriteLine ("true");
+else
+    Console.WriteLine ("false");
