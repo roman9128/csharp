74bd6787a6163e90ed9b88f649272044cbb9c6fe
roman <roma-t@bk.ru>
2024-01-01 15:39:42 +0300

    dz1 done

---
+++ dz1/task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+    int i = 2;
+    while (i <= number)
+        {
+            Console.Write(i + " "); 
+            i = i + 2;
+        }
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 3;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
