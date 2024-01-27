40e2fcdf5b8921361ce872be22ccad6222b4b7b3
roman <roma-t@bk.ru>
2024-01-27 18:49:09 +0300

    lesson_recursion

---
+++ lections/recursion/Program.cs
@@ -0,0 +1,9 @@
+﻿int FindFactorial(int number)
+{
+    if (number == 1 || number == 0)
+    {
+        return 1;
+    }
+    return number * FindFactorial(number - 1);
+}
+Console.WriteLine(FindFactorial(3));
