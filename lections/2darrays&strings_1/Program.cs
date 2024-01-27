09d9a9de201cb1ad97b8e8d6927f2200de9d0730
roman <roma-t@bk.ru>
2024-01-27 12:21:31 +0300

    lectionarr&str

---
+++ lections/2darrays&strings_1/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,] CreateMatrix(int row, int column)
+{
+    int[,] matrix = new int [row, column];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+void ShowMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] table = CreateMatrix (4,5);
+ShowMatrix(table);
